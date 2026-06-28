f4132861e37feda9cfb0412c766715c31685582d
yorndara <darayorn@gmail.com>
2026-06-29 05:19:51 +0700

    adminlte layout with basic navigation

--- vuejs-app/src/main.js
+++ vuejs-app/src/main.js
@@ -1,3 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'admin-lte/dist/css/adminlte.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
